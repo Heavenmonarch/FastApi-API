2091bfb017592d6dfc06016e3b53beb2e32e7c8d
AYANFE <heavenmonarc@gmail.com>
2026-03-19 13:04:43 +0100

    i

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ import uvicorn
 port = int(os.getenv("PORT", 8000))
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("main.app:app", host="0.0.0.0", port=port)
