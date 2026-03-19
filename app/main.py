64c562eff0f2d7de0f852d9d9be94e262cc3e4ea
AYANFE <heavenmonarc@gmail.com>
2026-03-19 11:27:02 +0100

    i

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
-import os
-import uvicorn
+# import os
+# import uvicorn
 
 
 
