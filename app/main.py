a085ecafd0a13f607ac00c2cec66f21d93e9d43a
AYANFE <heavenmonarc@gmail.com>
2026-03-19 11:26:36 +0100

    i

--- app/main.py
+++ app/main.py
@@ -38,7 +38,3 @@ def root():
     return {"message": "Homepage"}
 
 
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # fallback for local dev
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
